073629575df001ad106d8b14137725609155a917
Dmitry Mishin <dmitry.mishin@gmail.com>
2014-06-16 11:54:03 -0700

    Use right python in client

--- client/consumer.py
+++ client/consumer.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/opt/rocks/bin/python
 import logging
 import pika
 
--- client/send.py
+++ client/send.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/opt/rocks/bin/python
 import pika
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
