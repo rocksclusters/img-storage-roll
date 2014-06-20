a6c31a9d58cefb1587d0157074b53538ba974e96
Dmitry Mishin <dmitry.mishin@gmail.com>
2014-06-20 13:15:43 -0700

    Working roll, installs well. Did files renaming

--- src/img-storage-vm/setup.py
+++ src/img-storage-vm/setup.py
@@ -12,13 +12,13 @@ version = os.environ.get('ROCKS_VERSION')
 # main configuration of distutils
 # 
 setup(
-    name = 'rocks-pylib',
+    name = 'img-storage-fe',
     version = version,
-    description = 'Main Rocks python library',
-    author = 'Phil Papadopoulos',
-    author_email =  'philip.papadopoulos@gmail.com',
-    maintainer = 'Luca Clementi',
-    maintainer_email =  'luca.clementi@gmail.com',
+    description = 'Image Storage manager',
+    author = 'Dmitry Mishin',
+    author_email =  'dmitry.mishin@gmail.com',
+    maintainer = 'Dmitry Mishin',
+    maintainer_email =  'dmitry.mishin@gmail.com',
     platforms = ['linux'],
     url = 'https://rocksclusters.org',
     #long_description = long_description,
@@ -29,5 +29,5 @@ setup(
     # disable zip installation
     zip_safe = False,
     #the command line called by users    
-    scripts=['bin/daemon'],
+    scripts=['bin/rabbitmq-client-vm'],
 )
