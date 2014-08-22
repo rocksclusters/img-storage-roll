1255841219fb5d2d15073d50c2e7722964024a52
Luca Clementi <luca.clementi@gmail.com>
2014-08-22 15:50:57 -0700

    commands: fix identation
    use tab no spaces

--- src/rocks-command-imgstorage/rocks/commands/list/host/storagemap/__init__.py
+++ src/rocks-command-imgstorage/rocks/commands/list/host/storagemap/__init__.py
@@ -16,40 +16,40 @@ from imgstorage.commandlauncher import CommandLauncher
 
 
 class Command(rocks.commands.HostArgumentProcessor, rocks.commands.list.command):
-        """
-        List the current active mapping on a NAS (or virtual machine images 
-        repository).
-        
-        <arg type='string' name='nas' optional='0'>
-        The NAS name which we want to interrogate
-        </arg>
+	"""
+	List the current active mapping on a NAS (or virtual machine images 
+	repository).
+	
+	<arg type='string' name='nas' optional='0'>
+	The NAS name which we want to interrogate
+	</arg>
 
-        <example cmd='list host storagemap nas-0-0'>
-        It will display the list of mappings on nas-0-0
-        </example>
-        """
+	<example cmd='list host storagemap nas-0-0'>
+	It will display the list of mappings on nas-0-0
+	</example>
+	"""
 
-        #def list(self, nas):
-                # Im not too sure what is the best way to implement this
-                # what is the right set of api
+	#def list(self, nas):
+		# Im not too sure what is the best way to implement this
+		# what is the right set of api
 
-                # return list of tuple in the form of (zvolname, mappedhost, devicename)
-                #return [("zpool/vm-sdsc125-2","compute-0-0","/dev/sdc"), 
-                #        ("zpool/vm-sdsc125-3","compute-0-1","/dev/sdc")]
+		# return list of tuple in the form of (zvolname, mappedhost, devicename)
+		#return [("zpool/vm-sdsc125-2","compute-0-0","/dev/sdc"), 
+		#	("zpool/vm-sdsc125-3","compute-0-1","/dev/sdc")]
 
 
-        def run(self, params, args):
-                (args, nas) = self.fillPositionalArgs(('nas'))
+	def run(self, params, args):
+		(args, nas) = self.fillPositionalArgs(('nas'))
 
-                if not nas:
-                        self.abort("you must enter the nas name")
-                # debugging output
-                list = CommandLauncher().callListHostStoragemap(nas)
-                self.beginOutput()
-                for d in list:
-                    self.addOutput(nas, d.values())
-                headers=['nas','device', 'host', 'zvol']
-                self.endOutput(headers)
+		if not nas:
+			self.abort("you must enter the nas name")
+		# debugging output
+		list = CommandLauncher().callListHostStoragemap(nas)
+		self.beginOutput()
+		for d in list:
+			self.addOutput(nas, d.values())
+		headers=['nas','device', 'host', 'zvol']
+		self.endOutput(headers)
 
 
 
--- src/rocks-command-imgstorage/rocks/commands/remove/host/storagemap/__init__.py
+++ src/rocks-command-imgstorage/rocks/commands/remove/host/storagemap/__init__.py
@@ -9,39 +9,39 @@ import rocks.commands
 from imgstorage.commandlauncher import CommandLauncher
 
 class Command(rocks.commands.HostArgumentProcessor, rocks.commands.remove.command):
-        """
-        Remove a mapping between a virtual machine image from the NAS (or virtual machine images 
-        repository) to the hosting environment.
-        
-        <arg type='string' name='nas' optional='0'>
-        The NAS name which will host the storage image
-        </arg>
+	"""
+	Remove a mapping between a virtual machine image from the NAS (or virtual machine images 
+	repository) to the hosting environment.
+	
+	<arg type='string' name='nas' optional='0'>
+	The NAS name which will host the storage image
+	</arg>
 
-        <arg type='string' name='volume' optional='0'>
-        The volume name which will be unmapped on the hosting environment
-        </arg>
+	<arg type='string' name='volume' optional='0'>
+	The volume name which will be unmapped on the hosting environment
+	</arg>
 
-        <example cmd='remove host storagemap nas-0-0 zpool/vm-sdsc125-2'>
-        It removes the existing mapping on nas-0-0 zpool/vm-sdsc125-2
-        compute-0-0-0.
-        </example>
-        """
+	<example cmd='remove host storagemap nas-0-0 zpool/vm-sdsc125-2'>
+	It removes the existing mapping on nas-0-0 zpool/vm-sdsc125-2
+	compute-0-0-0.
+	</example>
+	"""
 
 
-        def run(self, params, args):
-                (args, nas, volume) = self.fillPositionalArgs(
-                                ('nas', 'volume'))
+	def run(self, params, args):
+		(args, nas, volume) = self.fillPositionalArgs(
+				('nas', 'volume'))
 
-                # debugging output
-                if not (nas and volume):
-                        self.abort("2 argument are required for this command nas volume")
+		# debugging output
+		if not (nas and volume):
+			self.abort("2 argument are required for this command nas volume")
 
-                # debugging output
-                print "unmapping  ", nas, ":", volume
-                CommandLauncher().callDelHostStoragemap(nas, volume)
+		# debugging output
+		print "unmapping  ", nas, ":", volume
+		CommandLauncher().callDelHostStoragemap(nas, volume)
 
-                self.beginOutput()
-                self.addOutput(nas, "Success")
-                self.endOutput(padChar='')
+		self.beginOutput()
+		self.addOutput(nas, "Success")
+		self.endOutput(padChar='')
 
 RollName = "img-storage"
