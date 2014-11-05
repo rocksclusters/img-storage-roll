474fde654e9b2cf238a1fd8b10a6260d18e62db1
Dmitry Mishin <dmitry.mishin@gmail.com>
2014-11-04 17:30:40 -0800

    New download process for rabbitmq-server

--- src/rabbitmq-server/version.mk
+++ src/rabbitmq-server/version.mk
@@ -1,4 +1,14 @@
-NAME        = rabbitmq-server
-VERSION     = 1.0
-RELEASE 	= 1
-TARBALL_POSTFIX	= tgz
+NAME               = rabbitmq-server
+PKGROOT            = /opt/rabbitmq-server
+VERSION            = 3.3.4
+RELEASE            = 1
+
+SRC_SUBDIR         = rabbitmq-server
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS           = $(SOURCE_PKG)
