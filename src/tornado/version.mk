977a46a9212cf182bef30c6eb490f3d608e0147c
Dmitry Mishin <dmitry.mishin@gmail.com>
2014-11-14 14:04:10 -0800

    All package archives are moved to Forge, makefiles are fixed

--- src/tornado/version.mk
+++ src/tornado/version.mk
@@ -1,9 +1,10 @@
-PKGROOT		   = usr
+PKGROOT		   = /opt/tornado
 NAME               = tornado
 VERSION            = 4.0.2
 RELEASE            = 1
 TARBALL_POSTFIX    = tar.gz
 
+SRC_SUBDIR         = tornado
 
 SOURCE_NAME        = $(NAME)
 SOURCE_VERSION     = $(VERSION)
@@ -11,3 +12,4 @@ SOURCE_SUFFIX      = tar.gz
 SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
 SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
+TAR_GZ_PKGS           = $(SOURCE_PKG)
