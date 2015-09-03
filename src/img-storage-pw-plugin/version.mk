8b025890d16ec9fc3485ba4865dc434bdbd4d2cc
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2015-09-03 10:44:30 -0700

    distribute the rabbitmq img storage password through the sec_attr channel instead of through 411

---
+++ src/img-storage-pw-plugin/version.mk
@@ -0,0 +1,7 @@
+NAME			= img-storage-pw-plugin
+VERSION			= 1
+RELEASE			= 1
+RPM.FILESLIST		= pkg-files
+PLUGIN			= img-storage_pw.py
+PLUGINDIR		= /opt/rocks/var/plugins/sec_attr
+RPM.REQUIRES		= rocks-secattr-plugins
