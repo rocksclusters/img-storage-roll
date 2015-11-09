ccd02f3cc90aa09a033dec161028893010363c11
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2015-11-09 11:55:00 -0800

    process img_sync attribute for creating a volume

--- src/rocks-command-imgstorage/rocks/commands/add/host/storagemap/__init__.py
+++ src/rocks-command-imgstorage/rocks/commands/add/host/storagemap/__init__.py
@@ -35,9 +35,19 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.add.command):
 	If the disk is already present on the NAS the size will be ignored.
 	</arg>
 
-	<example cmd='add host storagemap nas-0-0 tank vm-sdsc125-2 compute-0-0 35'>
+	<arg type='bool' name='img_sync' optional='1'>
+	Should the volume sync to the local (hosting) system or be an 
+        iscsi-only remote mount.  Default: uses remote host\'s 
+	img_sync attribute
+	</arg>
+
+	<example cmd='add host storagemap nas-0-0 tank vm-sdsc125-2 vm-container-0-0 35'>
 	If it does not exist create tank/vm-sdsc125-2 on nas and map it to 
-	compute-0-0-0.
+	vm-container-0-0.
+	</example>
+	<example cmd='add host storagemap nas-0-0 tank vm-sdsc125-2 vm-container 35 img_sync=no'>
+	If it does not exist create tank/vm-sdsc125-2 on nas and map it to vm-container-0-0. 
+	Do NOT sync the image (iscsi mount only)
 	</example>
 	"""
 
@@ -45,12 +55,17 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.add.command):
 		(args, nas, zpool, volume, hosting, size) = self.fillPositionalArgs(
 				('nas', 'zpool', 'volume', 'hosting', 'size'))
 
+		sync = self.fillParams( [('img_sync', None)] )
+	
 		if not nas or not zpool or not volume or not hosting or not size:
 			self.abort("you must pass 5 arguments nas_name zpool volume hosting size")
-
+		
+		if sync is None:
+			sync =  self.db.getHostAttr(hosting,'img_sync')
 		# debugging output
 		print "mapping ", nas, ":", zpool, "/", volume, " on ", hosting
-		device = CommandLauncher().callAddHostStoragemap(nas, zpool, volume, hosting, size)
+		device = CommandLauncher().callAddHostStoragemap(nas, zpool, volume, hosting, size, 
+			self.str2bool(sync) if sync is not None else False)
 		self.beginOutput()
 		self.addOutput(nas, device)
 		self.endOutput(padChar='')
